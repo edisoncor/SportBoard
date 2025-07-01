ef6d5a996830a78ba4d5a7967d37614fd603b84e
Edison Coronel <edisoncor@gmail.com>
2025-07-01 08:10:02 -0500

    feat: add footer, header, navigation, and sidebar components
    - Implemented Footer component with HTML, SCSS, and unit tests.
    - Created Header component with navigation links and user menu functionality.
    - Developed Navigation component for main navigation links.
    - Added Sidebar component with user information and navigation section.
    - Included unit tests for all new components to ensure functionality.

--- frontend/spa/src/app/app.ts
+++ frontend/spa/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Footer } from './shared/components/footer/footer';
+import { Header } from './shared/components/header/header';
+import { Sidebar } from './shared/components/sidebar/sidebar';
+import { SharedModule } from './shared/shared-module';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, Footer, Header, Sidebar, SharedModule],
+    templateUrl: './app.html',
+    styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'spa';
+    protected title = 'spa';
+    public sidebarOpened = true;
+
+    toggleSidebar() {
+        this.sidebarOpened = !this.sidebarOpened;
+    }
 }
--- frontend/spa/src/app/shared/shared-module.ts
+++ frontend/spa/src/app/shared/shared-module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { SharedRoutingModule } from './shared-routing-module';
-
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SharedRoutingModule
-  ]
+    declarations: [],
+    imports: [CommonModule],
+    exports: [MatSidenavModule, MatIconModule],
 })
-export class SharedModule { }
+export class SharedModule {}
