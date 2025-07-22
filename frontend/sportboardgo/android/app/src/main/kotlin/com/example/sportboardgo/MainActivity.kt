e01e765ead60bcd575a160f8edc6c2dc69298d5c
Edison Coronel <edisoncor@gmail.com>
2025-07-21 23:50:00 -0500

    Add Windows runner files for Flutter application
    - Created runner executable manifest to support DPI awareness and Windows 10/11 compatibility.
    - Implemented utility functions for console creation and command line argument handling in utils.cpp and utils.h.
    - Developed Win32Window class for managing a high DPI-aware window, including message handling, theme updates, and child content management in win32_window.cpp and win32_window.h.

---
+++ frontend/sportboardgo/android/app/src/main/kotlin/com/example/sportboardgo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sportboardgo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ frontend/sportboardref/android/app/src/main/kotlin/com/example/sportboardref/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sportboardref
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
